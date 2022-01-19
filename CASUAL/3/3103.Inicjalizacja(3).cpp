62bfb94e70938753febaef6cdd7b0ea52dcb8a9f
JGolanko <91740831+JGolanko@users.noreply.github.com>
2022-01-19 21:41:04 +0100

    Update 3103.Inicjalizacja(3).cpp

--- CASUAL/3/3103.Inicjalizacja(3).cpp
+++ CASUAL/3/3103.Inicjalizacja(3).cpp
@@ -1,19 +1,24 @@
 //INICJALIZACJA 3
-/*3.1.0.3 Inicjalizacja (3) Napisz program, który zainicjalizuje tablicê A
-o rozmiarze 50. Wype³nij tablicê przypadkowymi liczbami w zakresie
+/*3.1.0.3 Inicjalizacja (3) Napisz program, ktÃ³ry zainicjalizuje tablicÃª A
+o rozmiarze 50. WypeÂ³nij tablicÃª przypadkowymi liczbami w zakresie
 od 0 do k.*/
+
 #include <iostream>
+#include <ctime>
 using namespace std;
 int main()
 {
-	int tablica[50];
-	int k;
-	cout << "Podaj liczbe k ";
-	cin >> k;
-	for (int i = 0; i < 50; i++)
-	{
-		tabA[i] = { rand() % (k + 1) };
-		cout << "tablica[" << i << "] " << tablica[i];
-	}
-	return 0;
+    int k;
+    int i;
+    int o = 0;
+    cout << " Podaj liczbe k: " << endl;
+    cin >> k;
+
+    int A[50];
+    for (int i = 0; i < 50; i++)
+        A[i] = rand() % k + 1;
+
+    for (int i = 0; i < 50; i++)
+        cout << A[i] << endl;
+    return 0;
 }
