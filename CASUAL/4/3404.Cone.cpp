7b59750ed56ee4f81c2798a1949b1b9f00b574ef
JGolanko <91740831+JGolanko@users.noreply.github.com>
2022-01-19 23:47:31 +0100

    Update 3404.Cone.cpp

--- CASUAL/4/3404.Cone.cpp
+++ CASUAL/4/3404.Cone.cpp
@@ -8,15 +8,18 @@ using namespace std;
 
 void cone(int h, int r)
 {
-	double f = 3.14 * pow(2, r);
-	double v = (f / 3) * h;
-	cout << "Objętość stożka wynosi: " << v << "\n" << "Pole podstawy stożka wynosi: " << f << "\n";
+	double pp = 3.14 * pow(2, r);
+	double v = (pp*h)/3;
+	cout << "Objętość stożka wynosi: " << v << "\n" << "Pole podstawy stożka wynosi: " << pp << "\n";
 }
 
 int main()
 {
 	int h_value, r_value;
-	cout << "Podaj wysokość i promień stożka:"; cin >> h_value >> r_value;
+	cout << "Podaj wysokość stożka:"; 
+	cin >> h_value;
+	cout << "Podaj promień stożka:";
+	cin >> r_value;
 	int* h = &h_value;
 	int* r = &r_value;
 	cone(*h, *r);
