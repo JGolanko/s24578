fe58b01093609736cc891abd10c538218e3dfd08
JGolanko <91740831+JGolanko@users.noreply.github.com>
2022-01-19 21:37:44 +0100

    Update 3102.Inicjalizacja(2).cpp

--- CASUAL/3/3102.Inicjalizacja(2).cpp
+++ CASUAL/3/3102.Inicjalizacja(2).cpp
@@ -1,21 +1,35 @@
 //INICJALIZACJA 2
 /*
-3.1.0.2 Inicjalizacja (2) Napisz program, który zainicjalizuje tablicê A
-o rozmiarze n kolejnymi liczbami ca³kowitymi zaczynaj¹c od start.
-Przyk³ad: dla tablicy A[4] i start=10 tablica A zawiera³aby liczby 10, 11, 12, i 13.*/
+3.1.0.2 Inicjalizacja (2) Napisz program, ktÃ³ry zainicjalizuje tablicÃª A
+o rozmiarze n kolejnymi liczbami caÂ³kowitymi zaczynajÂ¹c od start.
+PrzykÂ³ad: dla tablicy A[4] i start=10 tablica A zawieraÂ³aby liczby 10, 11, 12, i 13.*/
+
 #include <iostream>
+#include <vector>
 using namespace std;
 int main()
 {
-	int tablica[100];
-	int n, start;
-	cin >> n;
-	cin >> start;
-	for (int i = start; i < n + start; i++)
-	{
-		tablica[i] = i;
-		cout << i;
-	}
+    int n;
+    int start;
+    cout << "podaj rozmiar tablicy: ";
+    cin >> n;
+    cout << "Podaj liczbe startowa: ";
+    cin >> start;
 
-	return 0;
+    if (n > 100) {
+        cout << "Liczba za duza" << endl;
+        return 0;
+    }
+    vector <int> A;
+    for (int i = 0; i < n; i++)
+    {
+        A.push_back(start);
+        start = start + 1;
+    }
+    for (int i = 0; i < A.size(); i++)
+    {
+        cout << A[i] << endl;
+    }
+
+    return 0;
 }
